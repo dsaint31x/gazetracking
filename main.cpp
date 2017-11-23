55e1793e755de02f7e7ae59fc16d24d5a4ee3b80
kyungrae <rlarudfo93@gmail.com>
2017-11-23 14:49:13 +0900

    show image

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ using namespace cv;
 
 int main(int argc, char* argv[])
 {
-	
+	Mat binary_frame = imread("findpupil.jpg", IMREAD_GRAYSCALE);
+	namedWindow("test", WINDOW_NORMAL);
+	imshow("test", binary_frame);
+
+	waitKey(0);
+
+	return 0;
 }
 
