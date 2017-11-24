97c496a6f2332205a75f992323e14e82cc1ec7a8
kyungrae <rlarudfo93@gmail.com>
2017-11-25 00:40:39 +0900

    nothing

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ using namespace cv;
 #define VERTICAL 1
 #define HORIZONTAL 2
 
-typedef struct ver_index_data
+typedef struct vIndex_data
 {
 	bool hasFPupil = false;
 	int first_pupil[2];
 	int second_pupil[2];
-}pupil_vertical_index_data;
+}vIndex_data;
 
-typedef struct hor_index_data
+typedef struct hIndex_data
 {
 	bool hasFPupil = false;
 	int first_pupil[2];
 	int second_pupil[2];
-}hor_index_data;
+}hIndex_data;
 
 void makeHistProj(Mat& src, Mat& dst, int direction);
 
@@ -29,133 +29,128 @@ int main(int argc, char* argv[])
 	namedWindow("test", WINDOW_NORMAL);
 	imshow("test", binary_frame);
 
-	//projection histogram을 위한 Mat 객체 생성
-	Mat binary_ver_proj(1, binary_frame.cols, CV_8U);
+	//finding indexing value using vertical histogram
+	vIndex_data kkr_vIndex;
+	Mat binary_vProj(1, binary_frame.cols, CV_8U);
+	makeHistProj(binary_frame, binary_vProj, VERTICAL);
+
+	for (int i = 1; i < binary_vProj.cols; i++)
+	{
+		if (kkr_vIndex.hasFPupil == false)
+		{
+			if (binary_vProj.at<uchar>(0, i) != 0 && binary_vProj.at<uchar>(0, i - 1) == 0)
+			{
+				kkr_vIndex.first_pupil[0] = i;
+			}
+			else if (binary_vProj.at<uchar>(0, i) == 0 && binary_vProj.at<uchar>(0, i - 1) != 0)
+			{
+				kkr_vIndex.first_pupil[1] = i - 1;
+				kkr_vIndex.hasFPupil = true;
+			}
+		}
+		else
+		{
+			if (binary_vProj.at<uchar>(0, i) != 0 && binary_vProj.at<uchar>(0, i - 1) == 0)
+			{
+				kkr_vIndex.second_pupil[0] = i;
+			}
+			else if (binary_vProj.at<uchar>(0, i) == 0 && binary_vProj.at<uchar>(0, i - 1) != 0)
+			{
+				kkr_vIndex.second_pupil[1] = i-1;
+				kkr_vIndex.hasFPupil = false;
+			}
+		}
+	}
+
+	//값 확인
+	cout << "(" << kkr_vIndex.first_pupil[0] << "," << kkr_vIndex.first_pupil[1] << ")" << endl;
+	cout << "(" << kkr_vIndex.second_pupil[0] << "," << kkr_vIndex.second_pupil[1] << ")" << endl;
 
 
-	//projection result
 
+	//make only left pupil Mat
+	Mat left_pupil = binary_frame.clone();
+	for (int i = 0; i < left_pupil.rows; i++)
+	{
+		uchar* p = left_pupil.ptr<uchar>(i);
+		for (int j = 0; j < left_pupil.cols; j++)
+		{
+			if (j >= kkr_vIndex.first_pupil[0] && j <= kkr_vIndex.first_pupil[1])
+			{
+				continue;
+			}
+			p[j] = 0;
+		}
+	}
 
-	//pupil_vertical_index_data kkr_ver_index;
-	//pupil_horizontal_index_data kkr_hor_index;
+	//finding indexing value using horizontal histogram
+	hIndex_data kkr_hIndex;
+	Mat left_hProj(1, left_pupil.rows, CV_8U);
+	makeHistProj(left_pupil, left_hProj, HORIZONTAL);
 
-	////indexing ver_proj
-	//for (int i = 1; i < ver_proj.cols; i++)
-	//{
-	//	if (kkr_ver_index.hasFPupil == false)
-	//	{
-	//		if (ver_proj.at<uchar>(0, i) != 0 && ver_proj.at<uchar>(0, i - 1) == 0)
-	//		{
-	//			kkr_ver_index.first_Lindex = i;
-	//		}
-	//		else if (ver_proj.at<uchar>(0, i) == 0 && ver_proj.at<uchar>(0, i - 1) != 0)
-	//		{
-	//			kkr_ver_index.first_Rindex = i - 1;
-	//			kkr_ver_index.hasFPupil = true;
-	//		}
-	//	}
-	//	else
-	//	{
-	//		if (ver_proj.at<uchar>(0, i) != 0 && ver_proj.at<uchar>(0, i - 1) == 0)
-	//		{
-	//			kkr_ver_index.second_Lindex = i;
-	//		}
-	//		else if (ver_proj.at<uchar>(0, i) == 0 && ver_proj.at<uchar>(0, i - 1) != 0)
-	//		{
-	//			kkr_ver_index.second_Rindex = i-1;
-	//			kkr_ver_index.hasFPupil = true;
-	//		}
-	//	}
-	//}
+	//indexing hor_proj
+	for (int i = 1; i < left_hProj.cols; i++)
+	{
+		if (left_hProj.at<uchar>(0, i) != 0 && left_hProj.at<uchar>(0, i - 1) == 0)
+		{
+			kkr_hIndex.first_pupil[0]= i;
+		}
+		else if (left_hProj.at<uchar>(0, i) == 0 && left_hProj.at<uchar>(0, i - 1) != 0)
+		{
+			kkr_hIndex.first_pupil[1] = i - 1;
+		}
+	}
 
-	//cout << "(" << kkr_ver_index.first_Lindex << "," << kkr_ver_index.first_Rindex << ")" << endl;
-	//cout << "(" << kkr_ver_index.second_Lindex << "," << kkr_ver_index.second_Rindex << ")" << endl;
+	cout << "(" << kkr_hIndex.first_pupil[0] << "," << kkr_hIndex.first_pupil[1] << ")" << endl;
 
-	////left pupil Mat
-	//Mat left_puppil_frame = binary_frame.clone();
-	//for (int i = 0; i < left_puppil_frame.rows; i++)
-	//{
-	//	uchar* p = left_puppil_frame.ptr<uchar>(i);
-	//	for (int j = 0; j < left_puppil_frame.cols; j++)
-	//	{
-	//		if (j >= kkr_ver_index.first_Lindex&&j <= kkr_ver_index.first_Rindex)
-	//		{
-	//			continue;
-	//		}
-	//		p[j] = 0;
-	//	}
-	//}
+	//make only right pupil Mat
+	Mat right_pupil = binary_frame.clone();
+	for (int i = 0; i < right_pupil.rows; i++)
+	{
+		uchar* p = right_pupil.ptr<uchar>(i);
+		for (int j = 0; j < right_pupil.cols; j++)
+		{
+			if (j >= kkr_vIndex.second_pupil[0] && j <= kkr_vIndex.second_pupil[1])
+			{
+				continue;
+			}
+			p[j] = 0;
+		}
+	}
 
-	////projection histogram을 위한 Mat 객체 생성
-	//Mat hor_left_proj(left_puppil_frame.rows, 1, CV_8U);
-	//hor_left_proj = Scalar::all(0);
+	//finding indexing value using horizontal histogram
+	Mat right_hProj(1, right_pupil.rows, CV_8U);
+	makeHistProj(right_pupil, right_hProj, HORIZONTAL);
 
-	////left eye projection result
-	//for (int i = 0; i < left_puppil_frame.rows; i++)
-	//{
-	//	hor_left_proj.at<uchar>(i, 0) = countNonZero(left_puppil_frame(Rect(0, i, left_puppil_frame.cols, 1)));
-	//}
+	Mat right_hProjR(right_pupil.rows, 1, CV_8U);
+	for (int i = 0; i < right_pupil.rows; i++)
+	{
+		right_hProjR
+	}
 
+	//indexing hor_proj
+	for (int i = 1; i < right_hProj.cols; i++)
+	{
+		if (right_hProj.at<uchar>(0, i) != 0 && right_hProj.at<uchar>(0, i - 1) == 0)
+		{
+			kkr_hIndex.second_pupil[0] = i;
+		}
+		else if (right_hProj.at<uchar>(0, i) == 0 && right_hProj.at<uchar>(0, i - 1) != 0)
+		{
+			kkr_hIndex.second_pupil[1] = i - 1;
+		}
+	}
 
-	////indexing hor_proj
-	//for (int i = 1; i < hor_left_proj.rows; i++)
-	//{
-	//	if (hor_left_proj.at<uchar>(i, 0) != 0 && hor_left_proj.at<uchar>(i - 1, 0) == 0)
-	//	{
-	//		kkr_hor_index.first_Tindex= i;
-	//	}
-	//	else if (hor_left_proj.at<uchar>(i, 0) == 0 && hor_left_proj.at<uchar>(i - 1, 0) != 0)
-	//	{
-	//		kkr_hor_index.first_Bindex = i - 1;
-	//	}
-	//}
+	cout << "(" << kkr_hIndex.second_pupil[0] << "," << kkr_hIndex.second_pupil[1] << ")" << endl;
 
-	//cout << "(" << kkr_hor_index.first_Tindex << "," << kkr_hor_index.first_Bindex << ")" << endl;
-
-	////right pupil Mat
-	//Mat right_puppil_frame = binary_frame.clone();
-	//for (int i = 0; i < right_puppil_frame.rows; i++)
-	//{
-	//	uchar* p = right_puppil_frame.ptr<uchar>(i);
-	//	for (int j = 0; j < right_puppil_frame.cols; j++)
-	//	{
-	//		if (j >= kkr_ver_index.second_Lindex&&j <= kkr_ver_index.second_Rindex)
-	//		{
-	//			continue;
-	//		}
-	//		p[j] = 0;
-	//	}
-	//}
-
-	////projection histogram을 위한 Mat 객체 생성
-	//Mat hor_right_proj(right_puppil_frame.rows, 1, CV_8U);
-	//hor_right_proj = Scalar::all(0);
-
-	////right eye projection result
-	//for (int i = 0; i < right_puppil_frame.rows; i++)
-	//{
-	//	hor_right_proj.at<uchar>(i, 0) = countNonZero(right_puppil_frame(Rect(0, i, right_puppil_frame.cols, 1)));
-	//}
-
-	////indexing hor_proj
-	//for (int i = 1; i < hor_right_proj.rows; i++)
-	//{
-	//	if (hor_right_proj.at<uchar>(i, 0) != 0 && hor_right_proj.at<uchar>(i - 1, 0) == 0)
-	//	{
-	//		kkr_hor_index.second_Tindex = i;
-	//	}
-	//	else if (hor_right_proj.at<uchar>(i, 0) == 0 && hor_right_proj.at<uchar>(i - 1, 0) != 0)
-	//	{
-	//		kkr_hor_index.second_Bindex = i - 1;
-	//	}
-	//}
-
-	//cout << "(" << kkr_hor_index.second_Tindex << "," << kkr_hor_index.second_Bindex << ")" << endl;
-
-	//namedWindow("left", WINDOW_NORMAL);
-	//imshow("left", left_puppil_frame);
-	//namedWindow("right", WINDOW_NORMAL);
-	//imshow("right", right_puppil_frame);
+	namedWindow("left", WINDOW_NORMAL);
+	imshow("left", left_pupil);
+	namedWindow("right", WINDOW_NORMAL);
+	imshow("right", right_pupil);
+	namedWindow("left_hist", WINDOW_NORMAL);
+	imshow("left_hist", left_hProj);
+	namedWindow("right_hist", WINDOW_NORMAL);
+	imshow("right_hist", right_hProj);
 
 
 	waitKey(0);
